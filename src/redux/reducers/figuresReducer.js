461b8f8bd3a60dc2dd2653c3dd37eeec443aefb0
Yuriy Ya <yy.ainu@gmail.com>
2020-12-29 16:18:33 +0300

    Update figures Reducer

--- src/redux/reducers/figuresReducer.js
+++ src/redux/reducers/figuresReducer.js
@@ -2,39 +2,38 @@ import config from '../../config/config'
 import { ADD_NEW_FIGURE, GET_ACTIVE_FIGURE, RESET_ACTIVE_FIGURE, SAVE_POSITION, GET_NEW_COLOR_TO_ACTIVE_FIGURE, KEY_LISTENER_DELETE } from "../actions/actionTypes"
 
 const initialState = localStorage.getItem(config.LOCAL_STORAGE_FIGURE)
-? {
-  figures: JSON.parse(localStorage.getItem(config.LOCAL_STORAGE_FIGURE)).figures,
-  maxId: JSON.parse(localStorage.getItem(config.LOCAL_STORAGE_FIGURE)).maxId,
-  activeFigure: null,
-}
-: {
-  figures: [],
-  maxId: 0,
-  activeFigure: null,
-}
+  ? {
+    figures: JSON.parse(localStorage.getItem(config.LOCAL_STORAGE_FIGURE)).figures,
+    maxId: JSON.parse(localStorage.getItem(config.LOCAL_STORAGE_FIGURE)).maxId,
+    activeFigure: null,
+  }
+  : {
+    figures: [],
+    maxId: 0,
+    activeFigure: null,
+  }
 
-export default function figuresReducer (state = initialState, action) {
+export default function figuresReducer(state = initialState, action) {
 
-  switch(action.type) {
-    case ADD_NEW_FIGURE:
-      const figures = state.figures
-      const maxId = state.maxId + 1
-      figures.push({
-        id: maxId,
-        type: action.name,
-        color: config.DEFAULT_FIGURE_COLOR,
-        position: {
-          top: 'calc(50% - 50px)',
-          left: 'calc(50% - 100px)'
-        }    
-      })
+  switch (action.type) {
+    case ADD_NEW_FIGURE: {
       return {
         ...state,
-        figures: figures,
-        maxId: maxId,
+        maxId: state.maxId + 1,
         activeFigure: null,
+        figures: [...state.figures, {
+          id: state.maxId + 1,
+          type: action.name,
+          color: config.DEFAULT_FIGURE_COLOR,
+          position: {
+            top: 'calc(50% - 50px)',
+            left: 'calc(50% - 100px)'
+          }
+        }],
       }
-    case GET_ACTIVE_FIGURE:
+    }
+
+    case GET_ACTIVE_FIGURE: {
       if (action.value.figure.id < state.maxId) {
         const figures = state.figures
         figures[action.value.index].id = state.maxId + 1
@@ -49,33 +48,49 @@ export default function figuresReducer (state = initialState, action) {
         ...state,
         activeFigure: action.value.figure
       }
+    }
+
     case RESET_ACTIVE_FIGURE:
       return {
         ...state,
         activeFigure: null
       }
-    case SAVE_POSITION:
-      const prevFigures = state.figures
-      prevFigures[action.index].position.top = action.top
-      prevFigures[action.index].position.left = action.left
+
+    case SAVE_POSITION: {
+      const figures = state.figures
+      figures[action.index].position.top = action.top
+      figures[action.index].position.left = action.left
       return {
         ...state,
-        figures: prevFigures
+        figures: figures
       }
-    case GET_NEW_COLOR_TO_ACTIVE_FIGURE:
+    }
+
+    case GET_NEW_COLOR_TO_ACTIVE_FIGURE: {
       const activeFigureId = state.activeFigure.id
-      const prevFigures2 = state.figures
-      const index = prevFigures2.findIndex(figure => figure.id === activeFigureId)
-      prevFigures2[index].color = action.color
+      // const figures = state.figures
+      // const index = figures.findIndex(figure => figure.id === activeFigureId)
+      // figures[index].color = action.color
+      // return {
+      //   ...state,
+      //   figures: figures
+      // }
       return {
         ...state,
-        figures: prevFigures2
+        figures: [...state.figures.map(f => {
+          if (f.id === activeFigureId) {
+            return { ...f, color: action.color }
+          }
+          return f
+        })]
       }
-    case KEY_LISTENER_DELETE:
+    }
+
+    case KEY_LISTENER_DELETE: {
       if (action.eCode === config.DELETE_KEY_DOWN) {
-        const prevFigures3 = state.figures
-        prevFigures3.splice(action.index, 1)
-        if (prevFigures3.length === 0) {
+        const figures = state.figures
+        figures.splice(action.index, 1)
+        if (figures.length === 0) {
           return {
             figures: [],
             maxId: 0,
@@ -84,11 +99,13 @@ export default function figuresReducer (state = initialState, action) {
         }
         return {
           ...state,
-          figures: prevFigures3,
+          figures: figures,
           activeFigure: null,
         }
       }
       return state
+    }
+
     default:
       return state
   }
